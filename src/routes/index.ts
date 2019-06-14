4955f3e52c67efc44ccb48f85c175218e844e883
Bruce Bigirwenkya <brucebigirwenkya@gmail.com>
2019-06-14 14:38:08 +0300

    Add healthcheck route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,5 +5,8 @@ export class Routes {
         app.get('/', (req: Request, res: Response) => {
             res.status(200).send({message: 'Welcome to the SMS TS API'});
         });
+        app.get('/healthcheck', (req: Request, res: Response) => {
+            res.status(200).send({message: 'ok'});
+        })
     }
 }
