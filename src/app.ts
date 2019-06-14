3bb04f925c44b1b9c2cc6b3ac366756c0373c484
Bruce Bigirwenkya <brucebigirwenkya@gmail.com>
2019-06-14 14:38:01 +0300

    Add mongodb connection

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import { Routes } from './routes';
+import config from './config/';
+import * as mongoose from 'mongoose';
 
 class App {
 
@@ -11,11 +13,23 @@ class App {
         this.app = express();
         this.config();
         this.routes.Routes(this.app);
+        this.mongoSetup();
     }
 
     private config(): void {
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({ extended: false }));
+    }
+
+    private mongoSetup(): void {
+        const connOptions = {
+            useNewUrlParser: true
+        }
+        try {
+            mongoose.connect(config.DB_URI, connOptions);
+        } catch (error) {
+            throw(`Could not connect to mongoDB: ${error}`);
+        }
     }
 
 }
