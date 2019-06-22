a8f03b5e1a8c2dfddcceea7099eaf67b0b3f81f5
Bruce Bigirwenkya <bruce.bigirwenkya@andela.com>
2019-06-22 19:53:22 +0300

    Update test structure config

--- src/__tests__/app.test.ts
+++ src/__tests__/app.test.ts
@@ -2,15 +2,25 @@ import app from '../app';
 import * as request from 'supertest';
 import * as mongoose from 'mongoose';
 
+let req: typeof request = null;
 describe('GET /', ()=> {
+    beforeAll(()=> {
+        req = request(app);
+    });
+
     afterAll(async done => {
         mongoose.disconnect();
         done();
     });
 
     it('returns welcome message', async()=> {
-        const res = await request(app).get('/');
+        const res = await req.get('/');
         expect(res.body).toHaveProperty('message');
         expect(res.body.message).toEqual('Welcome to the SMS TS API');
+    });
+
+    it('returns healthcheck alert', async () => {
+        const res = await req.get('/healthcheck');
+        expect(res.body.message).toBe('ok');
     });
 });
